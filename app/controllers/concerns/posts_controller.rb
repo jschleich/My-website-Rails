d159f75c9abbe556a1545ad45073d8440be2e5af
jschleich <schleichster@gmail.com>
2018-02-25 16:21:12 -0800

    uploads

---
+++ app/controllers/concerns/posts_controller.rb
@@ -0,0 +1,27 @@
+class PostsController < ApplicationController
+
+  #CRUD= create, read, update, delete
+
+  def create
+  end
+ 
+  def read
+  end 
+  
+  def update
+  end
+  
+  def show
+  end
+  
+  def index
+  end
+
+  def new
+  end
+   
+  def edit
+  end
+
+
+end   
